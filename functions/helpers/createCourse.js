3ba4c277dc83cf8dad390d69460c36c88d7a6896
Luca Di Fazio <highliuk@gmail.com>
2022-04-27 22:52:01 +0200

    ✨ POST Endpoint

--- functions/helpers/createCourse.js
+++ functions/helpers/createCourse.js
@@ -2,5 +2,8 @@ const { table } = require("./airtable")
 const formattedReturn = require("./formattedReturn")
 
 module.exports = async (event) => {
-  // TODO: create course
+  return table
+    .create([{ fields: JSON.parse(event.body) }])
+    .then((course) => formattedReturn(200, course))
+    .catch(() => formattedReturn(500, { msg: "Something went wrong" }))
 }
