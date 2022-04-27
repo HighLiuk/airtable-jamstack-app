ad1378f6c9393718344deb87af3bd1e3a3260784
Luca Di Fazio <highliuk@gmail.com>
2022-04-27 22:37:45 +0200

    ✨ GET Endpoint

--- functions/helpers/getCourses.js
+++ functions/helpers/getCourses.js
@@ -2,5 +2,15 @@ const { table } = require("./airtable")
 const formattedReturn = require("./formattedReturn")
 
 module.exports = async (event) => {
-  // TODO: get courses
+  return table
+    .select()
+    .firstPage()
+    .then((courses) =>
+      courses.map((course) => ({
+        id: course.id,
+        ...course.fields,
+      }))
+    )
+    .then((courses) => formattedReturn(200, courses))
+    .catch(() => formattedReturn(500, { msg: "Something went wrong" }))
 }
