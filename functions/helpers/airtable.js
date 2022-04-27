c4c63fdbf3ca66d6a219f7fafd2af67bac169194
Luca Di Fazio <highliuk@gmail.com>
2022-04-27 22:28:31 +0200

    🔧 Airtable

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,1 +1,10 @@
-// TODO: create Airtable configuration
+require("dotenv").config()
+
+const Airtable = require("airtable")
+const base = new Airtable({
+  apiKey: process.env.AIRTABLE_CLIENT_KEY,
+}).base(process.env.AIRTABLE_CLIENT_ID)
+
+module.exports = {
+  table: base("courses"),
+}
