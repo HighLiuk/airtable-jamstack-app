58c54ddbf923b3c006be40df30b2fdf7dab55bec
Luca Di Fazio <highliuk@gmail.com>
2022-04-27 22:28:18 +0200

    ✨ API Endpoints

--- functions/courses.js
+++ functions/courses.js
@@ -3,7 +3,14 @@ const getCourses = require("./helpers/getCourses")
 const createCourse = require("./helpers/createCourse")
 const deleteCourse = require("./helpers/deleteCourse")
 const updateCourse = require("./helpers/updateCourse")
+
+const routes = {
+  GET: getCourses,
+  POST: createCourse,
+  DELETE: deleteCourse,
+  PUT: updateCourse,
+}
+
 exports.handler = async (event) => {
-  // TODO: call appropriate helper function based on HTTP method
-  return formattedReturn(200, "Hello World")
+  return (await routes?.[event.httpMethod]?.(event)) ?? formattedReturn(405, {})
 }
