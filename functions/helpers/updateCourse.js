bda31280c5c86d7eea5d3550e0f2151f10aec0b3
Luca Di Fazio <highliuk@gmail.com>
2022-04-27 23:04:28 +0200

    ✨ PUT Endpoint

--- functions/helpers/updateCourse.js
+++ functions/helpers/updateCourse.js
@@ -2,5 +2,10 @@ const { table } = require("./airtable")
 const formattedReturn = require("./formattedReturn")
 
 module.exports = async (event) => {
-  // TODO: update course
+  const { id, ...fields } = JSON.parse(event.body)
+
+  return table
+    .update([{ id, fields }])
+    .then((course) => formattedReturn(200, course))
+    .catch(() => formattedReturn(500, { msg: "Something went wrong" }))
 }
